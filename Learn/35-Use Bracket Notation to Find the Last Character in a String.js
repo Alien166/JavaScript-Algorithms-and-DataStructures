c02c54da88dac8a9128866efdd2d0ad3232da2a4
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-06 16:42:04 +0300

    Create 35-Use Bracket Notation to Find the Last Character in a String.js

---
+++ Learn/35-Use Bracket Notation to Find the Last Character in a String.js
@@ -0,0 +1,6 @@
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+console.log(lastLetterOfLastName);
