dc501d19c10eaf419adf0e878df8fe0388049eac
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-06 16:30:27 +0300

    Create 32-Use Bracket Notation to Find the First Character in a String.js

---
+++ Learn/32-Use Bracket Notation to Find the First Character in a String.js
@@ -0,0 +1,7 @@
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+console.log(firstLetterOfLastName);
