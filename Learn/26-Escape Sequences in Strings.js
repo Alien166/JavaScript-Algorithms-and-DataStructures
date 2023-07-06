d63ce0e5044cb20c092243e36b9a3b59f9d1ab6e
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-06 15:07:58 +0300

    Create 26-Escape Sequences in Strings.js

---
+++ Learn/26-Escape Sequences in Strings.js
@@ -0,0 +1,15 @@
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+console.log(myStr)
+
+//
+
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\t	tab
+\r	carriage return
+\b	backspace
+\f	form feed
+//
