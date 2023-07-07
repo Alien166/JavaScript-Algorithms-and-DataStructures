0cd784f00bedbae2bad4f227c35655aae2b46b20
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-07 08:22:55 +0300

    Create 37-Word Blanks.js

---
+++ Learn/37-Word Blanks.js
@@ -0,0 +1,10 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The" +" " +myAdjective + " " +myNoun +" " +myVerb+" " +myAdverb+"."; 
+console.log(wordBlanks);
+// Change this line
+// Only change code above this line
