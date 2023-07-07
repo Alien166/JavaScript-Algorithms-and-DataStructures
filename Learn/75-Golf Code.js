5bed3667c869cf36aeef88e903661031fb1c1bdf
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-07 15:10:58 +0300

    Create 75-Golf Code.js

---
+++ Learn/75-Golf Code.js
@@ -0,0 +1,21 @@
+function golfScore(par, strokes) {
+  // Only change code below this line
+
+  if (strokes === 1) return "Hole-in-one!";
+
+  else if ((strokes - par) <= -2) return 'Eagle';
+
+  else if ((strokes - par) === -1) return 'Birdie';
+
+  else if (strokes === par) return 'Par';
+
+  else if ((strokes - par) === 1) return 'Bogey';
+
+  else if ((strokes - par) === 2) return 'Double Bogey';
+
+  else return 'Go Home!';
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(5, 1);
