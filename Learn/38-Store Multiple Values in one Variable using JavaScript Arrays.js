fabff94acce0056a5976d7e745b9e42e5f713aec
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-07 08:38:00 +0300

    Create 38-Store Multiple Values in one Variable using JavaScript Arrays.js

---
+++ Learn/38-Store Multiple Values in one Variable using JavaScript Arrays.js
@@ -0,0 +1,4 @@
+// Only change code below this line
+const myArray = ["Mohamed" , 1 , true];
+console.log(myArray);
+//  JavaScript allows you to store multiple data types in an array, making it a versatile and powerful data structure.
