7ab12ae3c7466b621aba8f52f17cce3e798966d5
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-07 11:12:50 +0300

    Create 48-Shopping List.js

---
+++ Learn/48-Shopping List.js
@@ -0,0 +1,7 @@
+const myList = [ ["Chocolate Bar", 15] ,
+                 [ "Mac"         , 16] ,
+                 [ " pepesi "    , 17] ,
+                 [ "mohamed"     , 18] ,
+                 ["ahmed"         , 20]
+               ];
+console.log(myList);
