d568c27ce56c8612f45c58e02419ba3b604870e5
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-08 12:33:27 +0300

    Create 94-Accessing Nested Arrays.js

---
+++ Learn/94-Accessing Nested Arrays.js
@@ -0,0 +1,21 @@
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+console.log(secondTree);
