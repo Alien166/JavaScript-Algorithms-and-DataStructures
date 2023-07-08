f38b5b172294ff178539b9699589658202c0b5a6
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-08 12:21:09 +0300

    Create 92-Manipulating Complex Objects.js

---
+++ Learn/92-Manipulating Complex Objects.js
@@ -0,0 +1,22 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CS",
+      "8T",
+      "LP" ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Bob",
+    "title": "Hello",
+    "release_year": 1950,
+    "formats": [
+      "CS",
+      "8T"
+    ]
+  }
+];
