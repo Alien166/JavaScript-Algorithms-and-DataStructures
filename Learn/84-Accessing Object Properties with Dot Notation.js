94fd18f3a0681db2205ae090ab8accbdbe6b8cf4
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-08 11:13:49 +0300

    Create 84-Accessing Object Properties with Dot Notation.js

---
+++ Learn/84-Accessing Object Properties with Dot Notation.js
@@ -0,0 +1,13 @@
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+console.log(hatValue+"\n" + shirtValue);
