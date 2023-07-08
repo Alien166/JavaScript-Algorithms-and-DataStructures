cd297f6553da1a303369b90ce0fa883bf0666956
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-08 11:30:21 +0300

    Create 88-Add New Properties to a JavaScript Object.js

---
+++ Learn/88-Add New Properties to a JavaScript Object.js
@@ -0,0 +1,13 @@
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark="woof"
+//or
+myDog["bark"]="woof"
+
+console.log(myDog.bark);
+
