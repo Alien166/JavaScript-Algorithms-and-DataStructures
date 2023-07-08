4ee3da67322ab079e6abf9d6c1738dcaa77bf86a
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-08 11:27:00 +0300

    Create 87-Updating Object Properties.js

---
+++ Learn/87-Updating Object Properties.js
@@ -0,0 +1,12 @@
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name="Happy Coder"
+// or
+myDog["name"]="Happy Coder";
