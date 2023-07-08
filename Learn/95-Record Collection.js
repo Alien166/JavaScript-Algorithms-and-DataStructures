4775c8b84b101fa09505727810a7066a3be06d95
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-08 14:06:40 +0300

    Create 95-Record Collection.js

---
+++ Learn/95-Record Collection.js
@@ -0,0 +1,34 @@
+var recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop === "tracks") {
+    records[id][prop] ??= [];
+    records[id][prop].push(value);
+  } else {
+    records[id][prop] = value;
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, "artist", "ABBA");
