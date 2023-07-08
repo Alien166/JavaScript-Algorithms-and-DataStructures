918849ad7252b144603f3bc75477c305272219c2
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-08 11:32:50 +0300

    Create 89-Delete Properties from a JavaScript Object.js

---
+++ Learn/89-Delete Properties from a JavaScript Object.js
@@ -0,0 +1,14 @@
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+
+delete myDog.tails;
+
+console.log(myDog);
