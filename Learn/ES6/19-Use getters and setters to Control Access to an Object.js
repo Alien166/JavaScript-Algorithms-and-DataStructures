c3d3aaf70a8b21a8e9a00d8f2cafdc0d1b7015ad
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-09 18:30:18 +0300

    Create 19-Use getters and setters to Control Access to an Object.js

---
+++ Learn/ES6/19-Use getters and setters to Control Access to an Object.js
@@ -0,0 +1,16 @@
+class Thermostat {
+  constructor(fahrenheit) {
+    this._tempInCelsius = 5/9 * (fahrenheit - 32);
+  }
+  get temperature(){
+    return this._tempInCelsius;
+  }
+  set temperature(newTemp){
+    this._tempInCelsius = newTemp;
+  }
+}
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
