ac4955774a9a33f14ed9d098b07b5d287dd1e64e
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-09 21:04:55 +0300

    Create 2637. Promise Time Limit.js

---
+++ 30-Days-of-JavaScript/day-17/2637. Promise Time Limit.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        const originalFnPromise = fn(...args);
+
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject('Time Limit Exceeded')
+            }, t);
+        })
+
+        return Promise.race([originalFnPromise, timeoutPromise]);
+        
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
