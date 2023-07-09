b2f3290ede84c0edc75d97b9836b058dbf8496cd
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-09 17:42:44 +0300

    Create 11-Use Destructuring Assignment to Assign Variables from Nested Objects.js

---
+++ Learn/ES6/11-Use Destructuring Assignment to Assign Variables from Nested Objects.js
@@ -0,0 +1,12 @@
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+  
+const  {today : {low :lowToday}} = LOCAL_FORECAST;
+const {today :{high :highToday}} = LOCAL_FORECAST;
+
+// Only change code above this line
