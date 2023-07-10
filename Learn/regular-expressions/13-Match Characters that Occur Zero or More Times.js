1c3c1848ece13e770ad46ac48484fbed66baab21
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-10 16:31:39 +0300

    Create 13-Match Characters that Occur Zero or More Times.js

---
+++ Learn/regular-expressions/13-Match Characters that Occur Zero or More Times.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+let chewieQuote="Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+console.log(result);
