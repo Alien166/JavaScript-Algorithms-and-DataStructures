960aacab5a107b9df44fbdbe7f55b5161c4e0ed7
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-10 18:05:32 +0300

    Create 32-Use Capture Groups to Search and Replace.js

---
+++ Learn/regular-expressions/32-Use Capture Groups to Search and Replace.js
@@ -0,0 +1,4 @@
+let str = "one two three";
+let fixRegex = /(\w+) (\w+) (\w+)/g; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
