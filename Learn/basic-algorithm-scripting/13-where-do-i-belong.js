9bd6145ce28fab25e0de526f940ef2a301df2363
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-13 14:28:46 +0300

    Create 13-where-do-i-belong.js

---
+++ Learn/basic-algorithm-scripting/13-where-do-i-belong.js
@@ -0,0 +1,13 @@
+function getIndexToIns(arr, num) {
+  arr = arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) {
+      return i;
+    }
+  }
+
+  return arr.length;
+}
+
+getIndexToIns([40, 60], 50);
