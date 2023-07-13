28e637a6d15ce40acf8bcbacfb79aa7847dabbbc
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-13 14:20:20 +0300

    Create 04-return-largest-numbers-in-arrays.js

---
+++ Learn/basic-algorithm-scripting/04-return-largest-numbers-in-arrays.js
@@ -0,0 +1,5 @@
+function largestOfFour(arr) {
+  return arr.map(subArr => Math.max.apply(null, subArr));
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
