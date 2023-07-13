62d70ac4eec2354d5b88498533c684ff7f43d9af
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-13 16:16:11 +0300

    Create 08-understand-own-properties.js

---
+++ Learn/object-oriented-programming/08-understand-own-properties.js
@@ -0,0 +1,19 @@
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+function getOwnProps (obj) {
+  const props = [];
+
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      props.push(prop);
+    }
+  }
+
+  return props;
+}
+
+const ownProps = getOwnProps(canary);
