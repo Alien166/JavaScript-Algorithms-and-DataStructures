82fee6001305de9c843cb4eaac3936280c6f3a4b
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-13 14:29:54 +0300

    Create 14-mutations.js

---
+++ Learn/basic-algorithm-scripting/14-mutations.js
@@ -0,0 +1,9 @@
+function mutation(arr) {
+  let hash = Object.create(null);
+
+  arr[0].toLowerCase().split('').forEach(c => hash[c] = true);
+
+  return !arr[1].toLowerCase().split('').filter(c => !hash[c]).length;
+}
+
+mutation(["hello", "hey"]);
