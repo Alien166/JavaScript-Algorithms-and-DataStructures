b0c90f0832c76bf2c2d3ef079543d11ecbec90bc
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-13 14:18:12 +0300

    Create 03-find-the-longest-word-in-a-string.js

---
+++ Learn/basic-algorithm-scripting/03-find-the-longest-word-in-a-string.js
@@ -0,0 +1,5 @@
+function findLongestWordLength(str) {
+  return str.split(' ').sort((a, b) => b.length - a.length)[0].length;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
