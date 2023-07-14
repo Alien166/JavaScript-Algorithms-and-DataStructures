aa96aec48d732fbac1e29154ee97ddf85e970748
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-14 15:25:15 +0300

    Create 25-use-an-iife-to-create-a-module.js

---
+++ Learn/object-oriented-programming/25-use-an-iife-to-create-a-module.js
@@ -0,0 +1,10 @@
+const funModule = (function () {
+  return {
+    isCuteMixin: obj => {
+      obj.isCute = () => true;
+    },
+    singMixin: obj => {
+      obj.sing = () => console.log("Singing to an awesome tune");
+    }
+  };
+})();
