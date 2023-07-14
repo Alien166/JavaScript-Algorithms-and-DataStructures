c7f9fcac8735b638c18fb80b05e4fbdc82e42051
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-14 15:23:05 +0300

    Create 22-use-a-mixin-to-add-common-behavior-between-unrelated-objects.js

---
+++ Learn/object-oriented-programming/22-use-a-mixin-to-add-common-behavior-between-unrelated-objects.js
@@ -0,0 +1,15 @@
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+function glideMixin (obj) {
+  obj.glide = () => 'Gliding!';
+}
+
+glideMixin(bird);
+glideMixin(boat);
