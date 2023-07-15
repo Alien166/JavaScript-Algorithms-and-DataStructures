21ab99a7a9cfd1603d8d09d877f2f5a3f9c4ce40
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-15 15:00:19 +0300

    Create 16-sort-an-array-alphabetically-using-the-sort-method.js

---
+++ Learn/functional-programming/16-sort-an-array-alphabetically-using-the-sort-method.js
@@ -0,0 +1,6 @@
+function alphabeticalOrder(arr) {
+  // Only change code below this line
+  return arr.sort();
+  // Only change code above this line
+}
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
