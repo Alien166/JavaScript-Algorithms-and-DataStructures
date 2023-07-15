b2572cc44709edfcd9d7dcd348fa877d9215fed4
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-15 14:43:45 +0300

    Create 05-refactor-global-variables-out-of-functions.js

---
+++ Learn/functional-programming/05-refactor-global-variables-out-of-functions.js
@@ -0,0 +1,19 @@
+// The global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+function add (bookList, bookName) {
+  return [...bookList, bookName];
+}
+
+function remove (bookList, bookName) {
+  const bookListCopy = [...bookList];
+  const bookNameIndex = bookList.indexOf(bookName);
+  if (bookNameIndex >= 0) {
+    bookListCopy.splice(bookNameIndex, 1);
+  }
+  return bookListCopy;
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
