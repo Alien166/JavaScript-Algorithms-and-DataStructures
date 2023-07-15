58b157a11a8cb27a9731d309dc28be85e52ffb47
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-15 14:54:50 +0300

    Create 09-implement-the-filter-method-on-a-prototype.js

---
+++ Learn/functional-programming/09-implement-the-filter-method-on-a-prototype.js
@@ -0,0 +1,11 @@
+Array.prototype.myFilter = function (callback) {
+  const newArray = [];
+  // Only change code below this line
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this) == true) {
+      newArray.push(this[i]);
+    }
+  }
+  // Only change code above this line
+  return newArray;
+};
