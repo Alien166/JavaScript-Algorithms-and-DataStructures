aa25bf833e1744f88a5382f62395c4cc5252bdd0
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-15 14:59:30 +0300

    Create 15-use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js

---
+++ Learn/functional-programming/15-use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js
@@ -0,0 +1,9 @@
+const squareList = (arr) => {
+  const positiveIntegers = arr.filter(num => {
+    return num >= 0 && Number.isInteger(num);
+  });
+  const squaredIntegers = positiveIntegers.map(num => {
+    return num ** 2;
+  });
+  return squaredIntegers;
+};
