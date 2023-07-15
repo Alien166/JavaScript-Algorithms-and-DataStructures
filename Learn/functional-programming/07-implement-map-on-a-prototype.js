58ee63ed042028da2e23375519bd25c8e429835c
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-15 14:52:57 +0300

    Create 07-implement-map-on-a-prototype.js

---
+++ Learn/functional-programming/07-implement-map-on-a-prototype.js
@@ -0,0 +1,9 @@
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this));
+  }
+  // Only change code above this line
+  return newArray;
+};
