ade8d6ac9485942944ccd707c0830ad002d88e3a
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-16 14:27:47 +0300

    Create 07-dna-pairing.js

---
+++ Learn/intermediate-algorithm-scripting/07-dna-pairing.js
@@ -0,0 +1,9 @@
+var lookup = Object.create(null);
+lookup.A = 'T';
+lookup.T = 'A';
+lookup.C = 'G';
+lookup.G = 'C';
+
+function pairElement(str) {
+ return str.split('').map(function(p) {return [p, lookup[p]];});
+}
