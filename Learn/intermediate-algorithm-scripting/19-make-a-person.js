bc8ff7ba46205bfa0bcad9d1c9d5161523f1f1ba
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-16 14:37:37 +0300

    Create 19-make-a-person.js

---
+++ Learn/intermediate-algorithm-scripting/19-make-a-person.js
@@ -0,0 +1,39 @@
+var Person = function(firstAndLast) {
+
+  var firstName, lastName;
+
+  function updateName(str) {
+    firstName = str.split(" ")[0];
+    lastName = str.split(" ")[1];
+  }
+
+  updateName(firstAndLast);
+
+  this.getFirstName = function(){
+    return firstName;
+  };
+
+  this.getLastName = function(){
+    return lastName;
+  };
+
+  this.getFullName = function(){
+    return firstName + " " + lastName;
+  };
+
+  this.setFirstName = function(str){
+    firstName = str;
+  };
+
+
+  this.setLastName = function(str){
+    lastName = str;
+  };
+
+  this.setFullName = function(str){
+    updateName(str);
+  };
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
