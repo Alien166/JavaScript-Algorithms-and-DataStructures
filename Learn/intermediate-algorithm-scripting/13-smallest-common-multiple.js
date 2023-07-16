486680616bc89f3b5e3c5fbd34af713f50e15116
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-16 14:31:44 +0300

    Create 13-smallest-common-multiple.js

---
+++ Learn/intermediate-algorithm-scripting/13-smallest-common-multiple.js
@@ -0,0 +1,19 @@
+function gcd(a, b) {
+    while (b !== 0) {
+        a = [b, b = a % b][0];
+    }
+    return a;
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
+function smallestCommons(arr) {
+  arr.sort(function(a,b) {return a-b;});
+  var rng = [];
+  for (var i = arr[0]; i <= arr[1]; i++) {
+    rng.push(i);
+  }
+  return rng.reduce(lcm);
+}
