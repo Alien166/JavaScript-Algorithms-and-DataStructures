e1e666d5c5218e3bbdf5932ae5bee8d16e880d14
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-16 14:27:12 +0300

    Create 06-search-and-replace.js

---
+++ Learn/intermediate-algorithm-scripting/06-search-and-replace.js
@@ -0,0 +1,8 @@
+function myReplace(str, before, after) {
+  if (before.charAt(0) === before.charAt(0).toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.substring(1);
+  } else {
+    after = after.charAt(0).toLowerCase() + after.substring(1);
+  }
+  return str.replace(before, after);
+}
