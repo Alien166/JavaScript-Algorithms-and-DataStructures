e2b3aca5c5e4dfe524a9d14625423fe3daa1d644
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-16 14:34:15 +0300

    Create 15-steamroller.js

---
+++ Learn/intermediate-algorithm-scripting/15-steamroller.js
@@ -0,0 +1,12 @@
+function steamrollArray(arr) {
+  if (!Array.isArray(arr)) {
+    return [arr];
+  }
+  var out = [];
+  arr.forEach(function(e) {
+    steamrollArray(e).forEach(function(v) {
+      out.push(v);
+    });
+  });
+  return out;
+}
