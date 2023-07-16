7ee2f377bd34d5b233264c4ec81d9c6c8c9f2520
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-16 14:29:16 +0300

    Create 09-sorted-union.js

---
+++ Learn/intermediate-algorithm-scripting/09-sorted-union.js
@@ -0,0 +1,14 @@
+function uniteUnique(arr) {
+  const args = [...arguments];
+  const result = [];
+  for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < args[i].length; j++) {
+      if (!result.includes(args[i][j])) {
+        result.push(args[i][j]);
+      }
+    }
+  }
+  return result;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
