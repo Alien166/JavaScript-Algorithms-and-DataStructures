a97f3554b3cdf53430dfb3177c072aaff52c6007
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-07-16 14:28:15 +0300

    Create 08-missing-letters.js

---
+++ Learn/intermediate-algorithm-scripting/08-missing-letters.js
@@ -0,0 +1,10 @@
+function fearNotLetter (str) {
+  for (var i = str.charCodeAt(0); i <= str.charCodeAt(str.length - 1); i++) {
+    var letter = String.fromCharCode(i);
+    if (str.indexOf(letter) === -1) {
+      return letter;
+    }
+  }
+
+  return undefined;
+}
